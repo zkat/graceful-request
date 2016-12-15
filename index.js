b394a4541b5d97836d880d8911463a9d0f357063
Kat Marchán <kzm@sykosomatic.org>
2016-12-14 19:27:11 -0800

    look at all the things we may literally never load

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('require-inject')('request', {})
+module.exports = require(
+  'require-inject'
+).installGlobally.andClearCache('request', {
+  hawk: require('./lib/hawk'),
+  'aws-sign2': require('./lib/aws-sign2'),
+  aws4: require('./lib/aws4'),
+  'forever-agent': require('./lib/forever-agent'),
+  'form-data': require('./lib/form-data'),
+  'http-signature': require('./lib/http-signature'),
+  'mime-types': require('./lib/mime-types'),
+  'request/lib/har': require('./lib/har'),
+  'request/lib/auth': require('./lib/auth'),
+  'request/lib/oauth': require('./lib/oauth'),
+  'request/lib/multipart': require('./lib/multipart'),
+  'request/lib/tunnel': require('./lib/tunnel')
+})
